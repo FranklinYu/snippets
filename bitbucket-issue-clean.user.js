830c439b3c0f98e26fd4c23d188fbc3793a9f3bc
Franklin Yu <franklinyu@hotmail.com>
2016-07-27 19:58:51 -0400

    Implement the function
    User should hide the comments with CSS (making it easier to unhide
    them).

--- bitbucket-issue-clean.user.js
+++ bitbucket-issue-clean.user.js
@@ -4,12 +4,24 @@
 // @version      0.1
 // @description  Block the useless "+1" comments.
 // @author       Franklin Yu
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @include      https://bitbucket.org/site/master/issues/*
 // @grant        none
 // ==/UserScript==
 
+// required features:
+//  - "for...of" statement
+//  - "let" statement
+
 (function() {
 	'use strict';
 
-	// Your code here...
+	let blackList = [];
+	for (let comment of $('.issue-comment').toArray()) {
+		if ($(comment).find('.comment-content').text().trim().startsWith('+1')) {
+			$(comment).addClass('spam-comment');
+			blackList.push(comment.id);
+		}
+	}
+	console.log("blocked comments:\n - " + blackList.join("\n - "));
 })();
