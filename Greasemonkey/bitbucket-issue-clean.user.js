fd57df20b2e86de3e191bec2735ead1770294989
Franklin Yu <franklinyu@hotmail.com>
2017-07-04 02:27:50 -0400

    Remove dependency on jQuery

--- Greasemonkey/bitbucket-issue-clean.user.js
+++ Greasemonkey/bitbucket-issue-clean.user.js
@@ -4,16 +4,16 @@
 // @version      0.2.1
 // @description  Block the useless "+1" comments.
 // @author       Franklin Yu
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @include      https://bitbucket.org/site/master/issues/*
 // @grant        none
 // ==/UserScript==
 
 let blackList = [];
-for (let comment of $('.issue-comment').toArray()) {
-	if ($(comment).find('.comment-content').text().trim().startsWith('+1')) {
-		$(comment).addClass('spam-comment');
-		blackList.push(comment.id);
+for (let comment of document.getElementsByClassName('issue-comment')) {
+	let content = comment.getElementsByClassName('comment-content')[0];
+	if (content.textContent.trim().startsWith('+1')) {
+		comment.classList.add('spam-comment');
+		blackList.push(comment);
 	}
 }
 console.info("blocked comments:\n", blackList);
