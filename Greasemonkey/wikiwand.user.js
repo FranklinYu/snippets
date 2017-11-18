94d254976980373ea3ceea8ab1df45e7a105a282
Franklin Yu <franklinyu@hotmail.com>
2017-11-18 18:01:48 -0500

    Add Greasemonkey script: Wikiwand

---
+++ Greasemonkey/wikiwand.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name Lightweight Wikiwand
+// @namespace https://franklinyu.name
+// @match https://*.wikipedia.org/*
+// @match https://*.wikipedia.org/w/index.php?*
+// @grant none
+// ==/UserScript==
+
+const lang = location.host.split('.')[0];
+
+let title, lang_var;
+if (location.pathname === '/w/index.php') {
+	title = new URLSearchParams(location.search).get('title');
+	lang_var = null;
+} else {
+	const path_segments = location.pathname.split('/');
+	title = path_segments[2];
+	if (path_segments[1] === 'wiki')
+		lang_var = null;
+	else
+		lang_var = path_segments[1];
+}
+
+const anchor = document.createElement('a');
+anchor.href = `https://www.wikiwand.com/${lang_var || lang}/${title}`;
+const image = document.createElement('img');
+image.src = 'https://upload.wikimedia.org/wikipedia/commons/4/4e/WikiWand_Logo.png';
+image.id = 'wikiwand-image';
+anchor.appendChild(image);
+document.getElementById('firstHeading').prepend(anchor);
+
+const style = document.createElement('style');
+style.type = 'text/css';
+style.appendChild(document.createTextNode('#wikiwand-image { width: 2em; }'));
+document.head.appendChild(style);
