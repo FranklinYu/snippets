ec0a422cddd00163d85a8722cadcabc3a72051ac
Franklin Yu <franklinyu@hotmail.com>
2018-12-07 16:38:28 -0500

    Fix IETF Datatracker script
    When using Greasemonkey 4 in Firefox, variables in `window` is not
    available in Greasemonkey script.

--- Greasemonkey/IETF-tracker.user.js
+++ Greasemonkey/IETF-tracker.user.js
@@ -1,9 +1,12 @@
 // ==UserScript==
-// @name Refined IETF Tracker
+// @name Refined IETF Datatracker
 // @namespace https://franklinyu.github.io
 // @match https://datatracker.ietf.org/doc/*
 // @grant none
 // ==/UserScript==
 
-const title = $('#content h2 small')
-title.wrap(`<a href="https://tools.ietf.org/html/${title.text()}"></a>`)
+const title = document.querySelector('#content h2 small')
+const anchor = document.createElement('a')
+title.replaceWith(anchor)
+anchor.append(title)
+anchor.href = `https://tools.ietf.org/html/${title.innerText}`
