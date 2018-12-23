611fd5d73438bece4495ab7565eddf8e6290a7ae
Franklin Yu <franklinyu@hotmail.com>
2018-12-22 22:16:43 -0500

    Simplify SSL Labs

--- Greasemonkey/ssllabs-refined.user.js
+++ Greasemonkey/ssllabs-refined.user.js
@@ -2,49 +2,20 @@
 // @name SSL Labs Refined
 // @description Improve SSL Labs
 // @namespace https://franklinyu.github.io
-// @version 0.1.0
+// @version 0.2.0
 // @include https://www.ssllabs.com/ssltest/analyze.html?*
 // @grant none
 // ==/UserScript==
 
 HTMLCollection.prototype.filter = Array.prototype.filter
-NodeList.prototype.find = Array.prototype.find
 
-/**
- * 
- * @param {HTMLElement} elem the header
- * @param {string} hash hexadecimal SHA-256
- */
-function addCertLinks(elem, hash) {
-  const crtSh = document.createElement('a')
-  crtSh.href = 'https://crt.sh/?q=' + hash
-  crtSh.innerText = 'crt.sh'
-
-  const censys = document.createElement('a')
-  censys.href = 'https://censys.io/certificates/' + hash
-  censys.innerText = 'Censys'
-
-  elem.append(' ', crtSh, ' ', censys)
-}
-
-/** @type {HTMLDivElement[]} */
-const certElements = document.getElementsByClassName('serverKeyCert')
+document.getElementsByClassName('serverKeyCert')
   .filter(e => e.innerText.trim() === 'Server Key and Certificate #1')
-for (const certElement of certElements) {
-  const table = certElement.closest('table')
-  /** @type {string} */
-  const hash = table.querySelector('.greySmall').innerText.match(/[0-9a-f]{64}/)[0]
-
-  addCertLinks(certElement, hash)
-
-  const row = table.querySelectorAll('tbody tr')
-    .find(tr => tr.cells[0].innerText.trim() === 'Certificate Transparency')
-  {
+  .map(e => e.closest('table').querySelector('.greySmall'))
+  .forEach(span => {
+    const hash = span.innerText.match(/[0-9a-f]{64}/)[0]
     const anchor = document.createElement('a')
-    const bytes = hash.match(/.{2}/g).map(str => parseInt(str, 16))
-    const b64 = encodeURIComponent(btoa(String.fromCharCode(...bytes)))
-    anchor.href = 'https://transparencyreport.google.com/https/certificates/' + b64
-    anchor.innerText = 'Google report'
-    row.cells[1].append(' ', anchor)
-  }
-}
+    anchor.href = 'https://crt.sh/?q=' + hash
+    anchor.innerText = hash
+    span.innerHTML = span.innerHTML.replace(hash, anchor.outerHTML)
+  })
